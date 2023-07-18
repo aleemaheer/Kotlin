5a685939d2080b25a2819145fcf386bcdfe046ee
aleem aheer <aleemaheer5769644@gmail.com>
2023-07-18 22:30:29 +0500

    Added if condition code

--- hello.kt
+++ hello.kt
@@ -1,6 +1,15 @@
 fun main() {
     println("Hello, There!")
-    println(addNumbres(1,2))
+    val a: Int = 1  // immediate assignment
+    val b = 2   // `Int` type is inferred
+    val c: Int  // Type required when no initializer is provided
+    c = 3       // deferred assignment
+
+    // variable declared by var can be reassigned or modified
+    var x = 5 // `Int` type is inferred
+    x += 1
+    // println(addNumbres(1,2))
+    runConditions(x, c)
 }
 
 // Function to add two numbers
@@ -8,11 +17,10 @@ fun addNumbres(num1: Int, num2: Int): Int {
     return num1 + num2
 }
 
-val a: Int = 1  // immediate assignment
-val b = 2   // `Int` type is inferred
-val c: Int  // Type required when no initializer is provided
-c = 3       // deferred assignment
-
-// variable declared by var can be reassigned or modified
-var x = 5 // `Int` type is inferred
-x += 1
+fun runConditions(x: Int, c: Int) {
+    if (x > c) {
+        println("$x is greater than $c")
+    } else {
+        println("$c is greater than $x")
+    }
+}
